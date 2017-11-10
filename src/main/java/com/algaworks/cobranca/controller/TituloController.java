5045f201a7477bcb961c333049b1886717de38c4
deusyvan <deusyvan@gmail.com>
2017-11-09 22:31:18 -0200

    Passamos a usar o ModelAndView como objeto para receber parametros

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
@@ -21,10 +22,11 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		
+	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
 		
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		return mv;
 	}
 }
