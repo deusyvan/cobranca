8ab55438afb1cc2e13b74ea5d607abf7e35fcee2
deusyvan <deusyvan@gmail.com>
2017-11-15 20:59:41 -0200

    Melhorando o codigo passando um metodo do model

--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -65,6 +65,10 @@ public class Titulo {
 		this.status = status;
 	}
 	
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
