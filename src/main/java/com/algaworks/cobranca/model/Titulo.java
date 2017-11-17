05d5ffdf3daf18702d8d99c508485b020a8d3d21
2º Sgt QE Deusyvan <deusyvan.silva@eb.mil.br>
2017-11-17 11:44:56 -0200

    Colocando outras validações

--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -11,8 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -23,13 +27,18 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotEmpty(message = "Descrição é obrigatória")
+	@Size(max=60, message= "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message = "Data de vencimento é obrigatória!")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
-	@NotNull(message = "Valor não pode ser nulo!") //Bean Validation
+	@NotNull(message = "Valor é obrigatório!") //Bean Validation
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999.99", message = "Valor não pode ser maior que 9.999.999,99!")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
